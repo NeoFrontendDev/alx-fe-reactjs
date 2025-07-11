553494517e247d5d65f244170965f812b51eccc9
Neo Mosebi <neosammosebi01@gmail.com>
2025-07-11 23:55:03 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +14,12 @@ function App() {
     <>
         <div>
           <WelcomeMessage />
+        </div>
+        <div>
+        <Header />
+        </div>
+      <div>
+        <MainContent />
         </div>
         <div>
         <a href="https://vite.dev" target="_blank">
@@ -33,6 +42,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
+    <div>
+     <Footer />   
+    </div>
   )
 }
 
