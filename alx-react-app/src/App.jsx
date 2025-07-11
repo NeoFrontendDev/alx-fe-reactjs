6c6bed8866cec6c32e5c417603303dd99eb2d335
Neo Mosebi <neosammosebi01@gmail.com>
2025-07-12 00:47:27 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
+import UserProfile from './UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,9 @@ function App() {
         </div>
       <div>
         <MainContent />
+        </div>
+        <div>
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         </div>
         <div>
         <a href="https://vite.dev" target="_blank">
