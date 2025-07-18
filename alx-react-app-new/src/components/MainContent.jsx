5f4348fe188e35d21120db27af89f1d86988f0dd
Neo Mosebi <neosammosebi01@gmail.com>
2025-07-18 23:07:04 +0200

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -4,6 +4,7 @@ function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#eef', textAlign: 'center' }}>
       <p>Welcome to the main content area of my favorite cities app!</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
