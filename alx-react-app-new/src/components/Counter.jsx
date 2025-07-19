92eec3cf28196cafbf94684771833873f5ca01b3
Neo Mosebi <neosammosebi01@gmail.com>
2025-07-19 22:52:51 +0200

    Update Counter.jsx

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -7,15 +7,15 @@ return (
   <div style={{ textAlign: 'center', marginTop: '40px' }}>
   <p style={{ fontSize: '24px' }}>Current Count: {count}
   </p>
-    <botton onClick={() =>
+    <button onClick={() =>
     setCount(count + 1)}
-      style={{margin: '5px' }}>Increment</botton>
-    <botton onClick={() =>
+      style={{margin: '5px' }}>Increment</button>
+    <button onClick={() =>
     setCount(count - 1)}
-      style={{margin: '5px' }}>Decrement</botton>
-    <botton onClick={() =>
+      style={{margin: '5px' }}>Decrement</button>
+    <button onClick={() =>
     setCount(0)}
-      style={{margin: '5px' }}>Reset</botton>
+      style={{margin: '5px' }}>Reset</button>
   </div>
   );
 }
