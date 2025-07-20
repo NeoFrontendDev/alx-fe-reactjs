5d6974187fe1474758621f9d51fe026ea87ed0c4
Neo <neosammosebi01@gmail.com>
2025-07-20 21:37:24 +0200

    my company directory created and added components for the website

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav style={{ padding: '10px', backgroundColour: '#333', color: '#fff'}}>
+        <Link to="/" style={{marginRight: '15px', color: '#fff' }}>Home</Link>
+        <Link to="/about" style={{marginRight: '15px', color: '#fff' }}>About</Link>
+        <Link to="/services" style={{marginRight: '15px', color: '#fff' }}>Services</Link>
+        <Link to="/contact" style={{marginRight: '15px', color: '#fff' }}>Contact</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
