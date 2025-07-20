52b1b75bce783cf586a6b7054b5ad89a89449636
Neo Mosebi <neosammosebi01@gmail.com>
2025-07-20 21:55:03 +0200

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <nav style={{ padding: '10px', backgroundColour: '#333', color: '#fff', display: 'flex', justifyContent: 'end' }}>
+        <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff', display: 'flex', justifyContent: 'end' }}>
         <Link to="/" style={{marginRight: '15px', color: '#fff' }}>Home</Link>
         <Link to="/about" style={{marginRight: '15px', color: '#fff' }}>About</Link>
         <Link to="/services" style={{marginRight: '15px', color: '#fff' }}>Services</Link>
