6465d59f0a4a961438780300fba54c1a493ba229
Neo <neosammosebi01@gmail.com>
2025-07-20 21:50:51 +0200

    installed react-dom in my company directory

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer() {
+    return (
+        <footer style={{ padding: '10px', backgroundColor: '#eee', textAlign: 'center' }}>
+            <p>(c) 2025 Our Company. All rights reserved.</p>
+        </footer>
+    );
+}
