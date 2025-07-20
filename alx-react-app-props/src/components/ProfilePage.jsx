07e8097a045f4a6694d4c8fcca90228b60850554
Neo Mosebi <neosammosebi01@gmail.com>
2025-07-20 19:37:22 +0200

    Update ProfilePage.jsx

--- alx-react-app-props/src/components/ProfilePage.jsx
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -1,9 +1,6 @@
-import React, { useContext } from 'react';
-import UserContext from './UserContext';
 import UserInfo from './UserInfo';
 
 function ProfilePage() {
-  const userData = useContext(UserContext);
   return <UserInfo />;
 }
 
