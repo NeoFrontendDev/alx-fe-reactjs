8b6885d6599a525b1516a785fab2c9bb6b1dca61
Neo Mosebi <neosammosebi01@gmail.com>
2025-07-26 00:59:17 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,6 +3,8 @@ import create from 'zustand'
 const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  deleteRecipe: (id) => set((state) => ({ recipes: state.recipe.filters((recipe) => recipe. })),
+  updateRecipe: (updateRecipe) => set((state) => ({ recipe: state.recipes.map((recipe) => recipe.id === updateRecipe.id ? updateRecipe), })),
   setRecipes: (recipes) => set({ recipes })
 }));
 
