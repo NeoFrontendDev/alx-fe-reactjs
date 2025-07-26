3137d54cdaa47b6e252db53af6ed5057c792e12f
Neo Mosebi <neosammosebi01@gmail.com>
2025-07-26 19:43:00 +0200

    Update recipeStore.js
    Updated Zustand Store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,11 +1,19 @@
-import create from 'zustand'
+import { create } from 'zustand';
 
-const useRecipeStore = create(set => ({
+export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  deleteRecipe: (id) => set((state) => ({ recipes: state.recipe.filters((recipe) => recipe. })),
-  updateRecipe: (updateRecipe) => set((state) => ({ recipe: state.recipes.map((recipe) => recipe.id === updateRecipe.id ? updateRecipe), })),
-  setRecipes: (recipes) => set({ recipes })
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, recipe],
+    })),
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
 }));
-
-export default useRecipeStore;
