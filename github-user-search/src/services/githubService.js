3e69433afd1879525a52dea033935146e64fe66b
Neo Mosebi <neosammosebi01@gmail.com>
2025-08-02 16:02:46 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/users";
+const SEARCH_URL = "https://api.github.com/search/users";
 
-export const fetchUserData = async (username) => {
-  const rescponse = await
-    axois.get('${BASE_URL}/${username}
-    );
-  return response.data;
+export const fetchAdvancedUsers = async ({ username, location, minRepos }) => {
+  let query = "";
+
+  if (username) query += ${username} in:login;
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>=${minRepos}`;
+
+  const response = await axios.get(${SEARCH_URL}?q=${encodeURIComponent(query)}&per_page=20);
+  return response.data.items;
 };
