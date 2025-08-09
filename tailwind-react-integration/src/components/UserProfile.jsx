01ce0776d6139e2ed4d16e314121ca0bf3ce6361
Neo Mosebi <neosammosebi01@gmail.com>
2025-08-09 23:57:09 +0200

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,16 +1,37 @@
-const UserProfile = ({ user }) => {
+import React from "react";
+
+const UserProfile = ({ name, bio, image }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-4 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto">
+    <div
+      className="
+        bg-white 
+        rounded-lg 
+        shadow-md 
+        hover:shadow-xl 
+        transition-shadow 
+        duration-300 
+        ease-in-out 
+        p-4 sm:p-4 md:p-8 
+        max-w-xs sm:max-w-xs md:max-w-sm 
+        mx-auto
+      "
+    >
       <img
-        src={user.avatar_url}
-        alt={`${user.login} avatar`}
-        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+        src={image}
+        alt={`${name}'s profile`}
+        className="rounded-full mx-auto w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 object-cover transition-transform 
+          duration-300 ease-in-out hover:scale-110"
       />
-      <h2 className="text-center font-bold sm:text-lg md:text-xl mt-4">
-        {user.name || user.login}
+      <h2
+        className="mt-4 text-center text-lg md:text-xl font-semibold transition-colors duration-300 
+          ease-in-out hover:text-blue-500"
+      >
+        {name}
       </h2>
-      <p className="text-center sm:text-sm md:text-base text-gray-600">
-        {user.bio || "No bio available"}
+      <p
+        className="mt-2 text-center text-sm md:text-base text-gray-600"
+      >
+        {bio}
       </p>
     </div>
   );
