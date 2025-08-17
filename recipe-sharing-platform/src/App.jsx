1d685934f5cacc6e04517fbe3fb75f9d4606501c
Neo Mosebi <neosammosebi01@gmail.com>
2025-08-17 19:56:47 +0200

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -5,6 +5,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import HomePage from './components/HomePage';
 import RecipeDetail from "./components/RecipeDetail";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,7 @@ function App() {
       <div className="min-h-screen bg-gray-100">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/add-recipe" element={<AddRecipeForm />} />
           <Route path="/recipe/:id" element={<RecipeDetail />} />
         </Routes>
       </div>
