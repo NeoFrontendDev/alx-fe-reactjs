b470e79fad32fbc36799bf805cde9045aff29b66
Neo <neosammosebi01@gmail.com>
2025-08-24 19:53:27 +0200

    new file added

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import PostsComponent from "./components/PostsComponent";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+      {/* Devtools to inspect queries/caching */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
